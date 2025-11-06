bce404faa40fea395aff3e0956194c761a1d338c
antoniovirgone <antonaccio86@gmail.com>
2025-11-06 17:35:55 +0100

    Initial Commit

---
+++ SmartFit/SmartFitApp.swift
@@ -0,0 +1,17 @@
+//
+//  SmartFitApp.swift
+//  SmartFit
+//
+//  Created by Antonio Virgone on 06/11/25.
+//
+
+import SwiftUI
+
+@main
+struct SmartFitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
